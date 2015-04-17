6e1fa7387e7d9f3bd40f8458deaf5ae60ab63997
Ryan2718 <ryanmforsyth@gmail.com>
2015-04-17 19:32:01 -0400

    Update Test.hs

--- HaskellGraph/Test.hs
+++ HaskellGraph/Test.hs
@@ -11,6 +11,7 @@ e3 = Edge "C" "A" 3
 
 directed = insertDirected e2 $ insertDirected e1 $ Graph []
 directedRemoved = removeDirected e2 directed
+directedAdded = insertDirected e3 directed
 
 test01 = True == graphContainsVertex "A" directed
 test02 = True == graphContainsVertex "B" directed
@@ -28,7 +29,29 @@ test10 = True == containsEdge e1 directedRemoved
 test11 = False == containsEdge e2 directedRemoved
 test12 = False == containsEdge e3 directedRemoved
 
+test13 = True == eulerianCycleExistsDirected directedAdded
+test14 = True == eulerianPathExistsDirected directedAdded
+
+test15 = False == eulerianCycleExistsDirected directed
+test16 = True == eulerianPathExistsDirected directed
+
+-- The Bridges of Konigsberg
+bridges = [ Edge "A" "B" 1 
+          , Edge "A" "B" 1
+          , Edge "A" "C" 1
+          , Edge "A" "D" 1
+          , Edge "A" "D" 1
+          , Edge "B" "C" 1
+          , Edge "D" "C" 1]
+
+konigsberg = foldr insertUndirected (Graph []) bridges
+
+testKonigsberg01 = False == eulerianCycleExistsUndirected konigsberg
+testKonigsberg02 = False == eulerianPathExistsUndirected konigsberg
+
 main = do
+          putStrLn "Started"
+          
           if test01 then return() else putStrLn "Failed test01"
           if test02 then return() else putStrLn "Failed test02"
           if test03 then return() else putStrLn "Failed test03"
@@ -41,4 +64,12 @@ main = do
           if test10 then return() else putStrLn "Failed test10"
           if test11 then return() else putStrLn "Failed test11"
           if test12 then return() else putStrLn "Failed test12"
+          if test13 then return() else putStrLn "Failed test13"
+          if test14 then return() else putStrLn "Failed test14"
+          if test15 then return() else putStrLn "Failed test15"
+          if test16 then return() else putStrLn "Failed test16"
+          
+          if testKonigsberg01 then return() else putStrLn "Failed testKonigsberg01"
+          if testKonigsberg02 then return() else putStrLn "Failed testKonigsberg02"
+          
           putStrLn "Finished"
